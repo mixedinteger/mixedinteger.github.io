7c51d8c24e033cc7a626939ba9b555ae6b11e001
Christopher Hojny <c.hojny@tue.nl>
2026-04-08 12:19:28 +0200

    add icon to indicate link

--- DOTs/js/aux_select_speakers.js
+++ DOTs/js/aux_select_speakers.js
@@ -112,6 +112,29 @@ export function create_speaker_table(speakers, container, bound) {
     titleCell.textContent = speaker.title;
     titleCell.style = "min-width: 250px;";
 
+    // add icon to show that this is a link
+    if ( speaker.abstracttext && speaker.abstracttext.trim() != "") {
+    const absLink = document.createElement("span");
+    absLink.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg"
+       width="16" height="16" viewBox="0 0 24 24"
+       fill="none" stroke="currentColor"
+       stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/>
+    <polyline points="15 3 21 3 21 9"/>
+    <line x1="10" y1="14" x2="21" y2="3"/>
+  </svg>
+`;
+
+    absLink.style.marginLeft = "6px";
+    absLink.style.cursor = "pointer";
+    absLink.style.verticalAlign = "middle";
+
+    titleCell.appendChild(absLink);
+
+    }
+
+
     // When clicked → show popup
     titleCell.addEventListener("click", () => {
       modalTitle.textContent = speaker.title;
